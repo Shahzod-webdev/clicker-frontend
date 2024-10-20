c53c82c74173ad29f2b44450a753ce47caababa0
Shahzod <bahodurovshahzod84@gmail.com>
2024-10-20 16:36:20 +0500

    starting to deploy

--- src/components/mining/mining.jsx
+++ src/components/mining/mining.jsx
@@ -5,7 +5,7 @@ const tg = window.Telegram.WebApp;
 const Mining =()=>{
     return(
         <div>
-            <h1>Mining</h1>
+            <h1>Mining2</h1>
             <span>{tg.initDataUnsafe.user.username}</span>
 
             <Avatar />
