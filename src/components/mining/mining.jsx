ba48b0b3f8af48c83f9aebaf8637e031f2ab1828
Shahzod <bahodurovshahzod84@gmail.com>
2024-10-20 17:20:24 +0500

    starting to deploy

--- src/components/mining/mining.jsx
+++ src/components/mining/mining.jsx
@@ -8,7 +8,7 @@ const Mining =()=>{
             <h1>Mining5</h1>
             <h1>{tg.initDataUnsafe.user.username}</h1>
             <Avatar />
-            <span>{tg.initDataUnsafe}</span>
+            <span>{tg.initDataUnsafe.user}</span>
         </div>
     )
 }
